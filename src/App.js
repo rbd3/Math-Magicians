447e59754fc7f7b59e8b15c51c8a79427a705e9b
rbd3 <rabedesanaa@gmail.com>
2023-07-04 11:31:22 +0300

    Fix linters error

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+      <h1>Math Magicians</h1>
       </header>
       <Calculator />
-    
     </div>
   );
 }
